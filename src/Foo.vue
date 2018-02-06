ae39deba06dd85e850b2473688708fc6c5c0d826
David Schmidt <mail@davidschmidt.at>
2018-02-06 12:08:32 +0100

    icons with vue-awesome

--- src/Foo.vue
+++ src/Foo.vue
@@ -1,10 +1,10 @@
+<template>
+  <div>foo<icon name="flag"></icon></div>
+</template>
+
 <script>
 export default {
   name: 'Foo',
   created: function() { this.$notify('foo'); }
 }
 </script>
-
-<template>
-  <div>foo</div>
-</template>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import notify from './notify.js'
 import BootstrapVue from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import 'vue-awesome/icons/flag' // import used icons to minimize build size
+import Icon from 'vue-awesome/components/Icon'
 
 Vue.use(BootstrapVue);
 
+Vue.component('icon', Icon)
 Vue.component('notify', notify);
 Vue.component('Foo', require('./Foo.vue'));
 
